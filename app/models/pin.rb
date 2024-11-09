0fc283a05bf5d6d97218371ee1a8243b59cebb63
peppei20240408 <ippei.kanoh@gmail.com>
2024-11-09 12:49:46 +0900

    各モデルにバリデーションを。

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,10 @@
 class Pin < ApplicationRecord
   belongs_to :toko_log
   has_many_attached :images
+
+  validates :order_of_pins, presence: true
+  validates :latitude,      presence: true
+  validates :longitude,     presence: true
+
+  validates :images,        presence: true
 end
--- app/models/toko_log.rb
+++ app/models/toko_log.rb
@@ -1,4 +1,8 @@
 class TokoLog < ApplicationRecord
   belongs_to :user
   has_many :pins
+
+  validates :title,       presence: true
+  validates :description, presence: true
+
 end
