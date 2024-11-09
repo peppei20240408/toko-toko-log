d2a00bde86229006a8ae9fb28d2b6dedee596b90
peppei20240408 <ippei.kanoh@gmail.com>
2024-11-09 19:03:32 +0900

    投稿機能の実装が完了

--- app/controllers/toko_logs_controller.rb
+++ app/controllers/toko_logs_controller.rb
@@ -1,11 +1,24 @@
 class TokoLogsController < ApplicationController
   def index
+    @toko_logs = TokoLog.all
   end
 
   def new
+    @toko_log = TokoLog.new
   end
 
   def create
+    @toko_log = TokoLog.new(toko_log_params)
+    if @toko_log.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def toko_log_params
+    params.require(:toko_log).permit(:title, :description).merge(user_id: current_user.id)
+  end
 end
